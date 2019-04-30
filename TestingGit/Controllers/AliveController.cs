d8224f22f542425ca3d94a9f0f8211fdea1b7511
Michael DeLuca <michael.deluca@zeninternet.co.uk>
2019-04-30 12:34:45 +0100

    initial commit

---
+++ TestingGit/Controllers/AliveController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestingGit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AliveController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("I'm OK!");
+        }
+    }
+}
